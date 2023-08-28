9a18a01319ee2824a5a598694fac076deb5eca0c
ahjoel <aholoujoel5@gmail.com>
2023-08-28 17:58:07 +0000

    integration de spring security update

--- src/main/java/com/dixmillescodeurs/poo/hello/config/SecurityConfiguration.java
+++ src/main/java/com/dixmillescodeurs/poo/hello/config/SecurityConfiguration.java
@@ -28,7 +28,7 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
-    private final LoginSuccessHandler loginSuccessHandler;
+    //private final LoginSuccessHandler loginSuccessHandler;
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final UserService userService;
